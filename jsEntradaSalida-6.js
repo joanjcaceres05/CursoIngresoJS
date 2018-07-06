b89479ce756015554c05f2e8a53a0a8a0ce6efd5
joanjcaceres05 <40859208+joanjcaceres05@users.noreply.github.com>
2018-07-06 12:06:21 -0300

    ejercicios curso de ingreso (6,7,8) 06/07

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,24 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{
+var numeroUno;
+var numeroDos;
+var sumar;
+
+//numeroUno="6";//
+//numerDos=6;//
+
+numeroUno=document.getElementById('numeroUno').value;
+numeroDos=document.getElementById('numeroDos').value;
+
+numeroUno=parseInt(numeroUno);
+numeroDos=parseInt(numeroDos);
+
+resultado=numeroUno+numeroDos;
+alert(resultado);
+
+
+}
+
