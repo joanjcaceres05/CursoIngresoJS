6dd2363eb804d66257311b26d1a82a5dfebaf86c
joanjcaceres05 <40859208+joanjcaceres05@users.noreply.github.com>
2018-07-06 01:25:29 -0300

    primer commit

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,8 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	nombre = prompt("ingrese su nombre aqui");
+	alert("su nombre es " + nombre);
+}
+
