71a20fc8b913e447a0fa559a82b81820376dcee0
joanjcaceres05 <40859208+joanjcaceres05@users.noreply.github.com>
2018-07-06 12:05:20 -0300

    ejercicios parcial(2,3) 6/07

---
+++ tres.js
@@ -0,0 +1,20 @@
+function mostrar()
+{
+    var precio;
+    var descuento;
+
+    precio=prompt("ingrese su precio");
+    descuento=prompt("ingrese su descuento");
+
+    descuento=(precio%descuento);
+
+    alert("su descuento es de" + precio%descuento);
+
+
+
+   
+    
+
+
+
+}
